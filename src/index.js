43bc913e9aba089a8111c16ff890908635adb505
arunrajbhar123 <arunrajbhar1998@gmai.com>
2022-04-29 05:38:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import home from '../components/home';
+import '../styles/home.css';
+let user_add = document.querySelector('#user_add');
+let movie = document.querySelector('#section-first');
+movie.innerHTML = home();
